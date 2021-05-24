915c2757136a818042bc04a5e580a4411929f96d
YUNI0107 <amy123chou@gmail.com>
2021-05-24 23:02:33 +0800

    修正修正Nav+scroll

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,31 @@
 <template>
   <div id="app">
-    <NavBar/>
-    <InfoModal/>
-    <router-view/>
+    <NavBar />
+    <InfoModal />
+    <router-view />
   </div>
 </template>
 
 <script>
-import NavBar from './components/NavBar/NavBar'
-import InfoModal from './components/InfoModal/InfoModal'
-import smoothscroll from 'smoothscroll-polyfill'
+import NavBar from "./components/NavBar/NavBar";
+import InfoModal from "./components/InfoModal/InfoModal";
+import smoothscroll from "smoothscroll-polyfill";
+
+smoothscroll.polyfill();
 
 export default {
-  components:{
+  components: {
     NavBar,
     InfoModal,
   },
-  mounted() {
-    smoothscroll.polyfill();
-  },
-}
+};
 </script>
 
+<style>
+
+html,
+body {
+  scroll-behavior: smooth;
+}
+
+</style>
